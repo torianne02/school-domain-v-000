f0e8785a4527def9201f97e614c0f954f3c9341a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-09 17:52:46 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,11 @@
 class School
+  attr_accessor :name, :grade
   attr_reader :roster
 
  def initialize(roster)
    @roster ||= {}
  end
 
+  def add_student(name, grade)
+    roster[:name][:grade]
 end
